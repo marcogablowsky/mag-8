f41a508a06565988688d31f8e9cae052c6ff0db0
marco <m.gablowsky@googlemail.com>
2015-02-24 22:23:27 +0100

    added vm tests; restructured test layout

--- test/memory.spec.js
+++ test/memory.spec.js
@@ -6,58 +6,67 @@ describe('MAG8 memory:', function(){
         mem = new MAG.mag8.Memory;
     });
 
-    it('should define a reset method', function(){
-        expect(mem.reset).toBeDefined();
-        expect(typeof mem.reset).toEqual('function');
+    describe('interface',function() {
+        it('should define a reset method', function(){
+            expect(mem.reset).toBeDefined();
+            expect(typeof mem.reset).toEqual('function');
+        });
+
+        it('should define a store method', function(){
+            expect(mem.store).toBeDefined();
+            expect(typeof mem.store).toEqual('function');
+        });
+
+        it('should define a get method', function(){
+            expect(mem.get).toBeDefined();
+            expect(typeof mem.get).toEqual('function');
+        });
     });
 
-    it('should define a store method', function(){
-        expect(mem.store).toBeDefined();
-        expect(typeof mem.store).toEqual('function');
-    });
+    describe('behaviour',function() {
+        it('should store valid values',function(){
+            mem.store(0xff,0xfff);
+            expect(mem.get(0xfff)).toEqual(0xff);
+            mem.store(0x9,0x0);
+            expect(mem.get(0x0)).toEqual(0x9);
+        });
 
-    it('should define a get method', function(){
-        expect(mem.get).toBeDefined();
-        expect(typeof mem.get).toEqual('function');
-    });
+        it('should be initialized with zeros after creation',function(){
+            for(var i=0x0; i < 0x1000; i++){
+                expect(mem.get(i)).toEqual(0x0);
+            }
+        });
 
-    it('should store valid values',function(){
-        mem.store(0xff,0xfff);
-        expect(mem.get(0xfff)).toEqual(0xff);
-        mem.store(0x9,0x0);
-        expect(mem.get(0x0)).toEqual(0x9);
-    });
+        it('should reset all memory slots to 0 on a call to reset',function(){
+            for(var i=0x0; i < 0x1000; i++){
+                mem.store(0xff,i);
+            }
+            mem.reset();
+            for(i=0x0; i < 0x1000; i++){
+                expect(mem.get(i)).toEqual(0x0);
+            }
+        });
 
-    it('should throw errors if store address is invalid',function(){
-        expect(function(){
-            mem.store(0xA,0x1000)
-        }).toThrowError('Invalid address 4096');
-    });
+        it('should throw errors if store address is invalid',function(){
+            expect(function(){
+                mem.store(0xA,0x1000)
+            }).toThrowError('Invalid address 4096');
+        });
 
-    it('should throw errors if store value is invalid',function(){
-        expect(function(){
-            mem.store(-1,0xff)
-        }).toThrowError('Invalid value -1');
+        it('should throw errors if store value is invalid',function(){
+            expect(function(){
+                mem.store(-1,0xff)
+            }).toThrowError('Invalid value -1');
 
-        expect(function(){
-            mem.store(0x100,0xff)
-        }).toThrowError('Invalid value 256');
-    });
+            expect(function(){
+                mem.store(0x100,0xff)
+            }).toThrowError('Invalid value 256');
+        });
 
-    it('should throw errors if get address is invalid',function(){
-        expect(function(){
-            mem.get(0x1000)
-        }).toThrowError('Invalid address 4096');
-    });
-
-    it('should reset all memory slots to 0 on a call to reset',function(){
-        var i;
-        for(i=0x0; i < 0x1000; i++){
-            mem.store(0xff,i);
-        }
-        mem.reset();
-        for(i=0x0; i < 0x1000; i++){
-            expect(mem.get(i)).toEqual(0x0);
-        }
+        it('should throw errors if get address is invalid',function(){
+            expect(function(){
+                mem.get(0x1000)
+            }).toThrowError('Invalid address 4096');
+        });
     });
 });
---
+++ test/vm.spec.js
@@ -0,0 +1,49 @@
+describe('MAG8 virtual machine:', function() {
+
+    var vm;
+
+    beforeEach(function () {
+        vm = MAG.mag8.VM;
+    });
+
+    describe('interface', function() {
+        it('should define a reset method', function () {
+            expect(vm.reset).toBeDefined();
+            expect(typeof vm.reset).toEqual('function');
+        });
+
+        it('should define a start method', function () {
+            expect(vm.start).toBeDefined();
+            expect(typeof vm.start).toEqual('function');
+        });
+
+        it('should define a stop method', function () {
+            expect(vm.stop).toBeDefined();
+            expect(typeof vm.stop).toEqual('function');
+        });
+
+        it('should define an isRunning method', function () {
+            expect(vm.isRunning).toBeDefined();
+            expect(typeof vm.isRunning).toEqual('function');
+        });
+    });
+
+    describe('behaviour', function() {
+        it('should not be running right after creation',function(){
+            expect(vm.isRunning()).toBeFalsy();
+        });
+
+        it('should switch states through start / stop calls', function(){
+            vm.start();
+            expect(vm.isRunning()).toBeTruthy();
+            vm.stop();
+            expect(vm.isRunning()).toBeFalsy();
+        });
+
+        it('should not be running after a reset call', function(){
+            vm.start();
+            vm.reset();
+            expect(vm.isRunning()).toBeFalsy();
+        });
+    });
+});
