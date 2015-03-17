940d389bb63fa6e2a6bad66e14bee1ee6ec3d56b
marcogablowsky <m.gablowsky@googlemail.com>
2015-03-17 12:12:22 +0100

    added simple gulp build

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+var bases = {
+    app: '/',
+    dist: 'dist/'
+};
+
+var paths = {
+    scripts: ['src/**/*.js','!src/scripts/globals.js'],
+    scriptsTarget: '/scripts',
+    html: ['index.html']
+};
+
+// Delete the dist directory
+gulp.task('clean', function () {
+    return gulp.src(bases.dist)
+        .pipe(clean());
+});
+
+gulp.task('jshint', ['clean'], function () {
+    gulp.src(paths.scripts)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('scripts-min', ['clean'], function () {
+    gulp.src(paths.scripts)
+        .pipe(concat('mag-8.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(bases.dist + paths.scriptsTarget));
+});
+
+gulp.task('scripts', ['clean'], function () {
+    gulp.src(paths.scripts)
+        .pipe(concat('mag-8.js'))
+        .pipe(gulp.dest(bases.dist + paths.scriptsTarget));
+});
+
+gulp.task('default', ['clean', 'jshint', 'scripts', 'scripts-min']);
