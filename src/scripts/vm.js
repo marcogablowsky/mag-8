a8a36d12d530a3d085fc64a4242c207adc9d5462
marco <m.gablowsky@googlemail.com>
2015-03-20 19:59:49 +0100

    implemented main loop and keyboard event handlers

--- src/scripts/vm.js
+++ src/scripts/vm.js
@@ -1,11 +1,14 @@
 'use strict';
 
-MAG.mag8.VM = function(debug) {
+MAG.mag8.VM = function(renderer, debug) {
 
     var memory = MAG.mag8.Memory('8-bit',0x1000);
     var display = MAG.mag8.Display();
     var controls = MAG.mag8.Controls();
     var cpu = MAG.mag8.CPU(memory,display,controls);
+
+    // basic clock is at 60Hz -> through requestAnimationFrame
+    var clockMultiplier = 29; // -> 1.74 MhZ
 
     var running = false;
 
@@ -15,7 +18,24 @@ MAG.mag8.VM = function(debug) {
 
     var _initMemory = function(){
         for(var i = 0; i < MAG.mag8.Constants.hexChars.length; i++){
-            memory.store(i,MAG.mag8.Constants.hexChars[i]);
+            memory.store(MAG.mag8.Constants.hexChars[i],i);
+        }
+    };
+
+    var _loop = function(cb) {
+        var _cb = function() { cb(); requestAnimationFrame(_cb);};
+        _cb();
+    };
+
+    var _runInternal = function() {
+        if (running) {
+            for (var i = 0; i < clockMultiplier; i++) {
+                cpu.emulateCycle();
+            }
+            if (renderer) {
+                renderer.render(display);
+            }
+            cpu.handleTimers();
         }
     };
 
@@ -27,8 +47,15 @@ MAG.mag8.VM = function(debug) {
         _initMemory();
     };
 
+    var resetCpuOnly = function(){
+        cpu.reset();
+    };
+
     var step = function(){
         cpu.emulateCycle();
+        if (renderer) {
+            renderer.render(display);
+        }
     };
 
     var start = function(){
@@ -49,16 +76,35 @@ MAG.mag8.VM = function(debug) {
         }
     };
 
+    var setClockMultiplier = function(factor){
+        clockMultiplier = factor;
+    };
+
+    var keyPressed = function(key){
+        controls.keyPressed(key);
+    };
+
+    var keyReleased = function(key){
+        controls.keyReleased(key);
+    };
+
     // initialize memory on creation
     _initMemory();
 
+    //kick off the main loop
+    _loop(_runInternal);
+
     return {
         reset: reset,
+        resetCpuOnly: resetCpuOnly,
         step: step,
         start: start,
         stop: stop,
         isRunning: isRunning,
-        loadProgram: loadProgram
+        loadProgram: loadProgram,
+        setClockMultiplier: setClockMultiplier,
+        keyPressed: keyPressed,
+        keyReleased: keyReleased
     };
 
 };
