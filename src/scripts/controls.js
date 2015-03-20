79cbae861834b70e573ef0a8083e8896921912b7
marco <m.gablowsky@googlemail.com>
2015-03-20 19:55:38 +0100

    added key mapping to controls

--- src/scripts/controls.js
+++ src/scripts/controls.js
@@ -4,6 +4,25 @@ MAG.mag8.Controls = function(){
 
     var keystates = new Array(0x10);
 
+    var _keyMap = {
+        49: 0x1,  // 1
+        50: 0x2,  // 2
+        51: 0x3,  // 3
+        52: 0xC,  // 4
+        81: 0x4,  // Q
+        87: 0x5,  // W
+        69: 0x6,  // E
+        82: 0xD,  // R
+        65: 0x7,  // A
+        83: 0x8,  // S
+        68: 0x9,  // D
+        70: 0xE,  // F
+        90: 0xA,  // Z
+        88: 0x0,  // X
+        67: 0xB,  // C
+        86: 0xF   // V
+    };
+
     var _checkInput = function(keycode){
         if(keycode < 0x0 || keycode > 0xf){
             throw new Error('Invalid keycode '+keycode+ '. Must be 0-15');
@@ -17,13 +36,15 @@ MAG.mag8.Controls = function(){
     };
 
     var keyPressed = function(keycode){
-        _checkInput(keycode);
-        keystates[keycode] = true;
+        var key =_keyMap[keycode];
+        _checkInput(key);
+        keystates[key] = true;
     };
 
     var keyReleased = function(keycode){
-        _checkInput(keycode);
-        keystates[keycode] = false;
+        var key =_keyMap[keycode];
+        _checkInput(key);
+        keystates[key] = false;
     };
 
     var isPressed = function(keycode){
